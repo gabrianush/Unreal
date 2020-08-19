4652f8a6df7f4b12ac661963cc30704851c2c82d
GabrielHromei <gabrielhromei731@gmail.com>
2020-08-19 09:50:39 +0300

    Initial Commit

---
+++ BuildEscape/Source/BuildEscape/BuildEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDESCAPE_API ABuildEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
